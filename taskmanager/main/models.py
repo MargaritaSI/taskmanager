68d905b534a77123bfe9d9ec028e47771bae9ae6
Margarita Smyslava <madgeSmy@protonmail.com>
2023-01-10 14:44:05 +0100

    edit base.html

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField('Name', max_length=50)
+    task = models.TextField('Description')
+
+    def __str__(self): #call this method when we try to show on screen object of this class
+        return self.title
--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index), #when we go to the main page - we take method from views and show users html
-    path("about-us", views.about),
+    path("", views.index, name='home'), #when we go to the main page - we take method from views and show users html
+    path("about-us", views.about, name='about'),
 ]
